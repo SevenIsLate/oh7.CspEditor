879032f3f0d4c3c54473ff562041f3cd5b0b0161
Øistein Hay Syversen <oistein.h.syversen@gmail.com>
2021-04-12 10:46:27 +0200

    Remove old project

--- oh7.CspEditor.WebApp/Pages/BasePageModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace oh7.CspEditor.WebApp.Pages
-{
-    public class BasePageModel : PageModel
-    {
-        public string GetUserId => User.GetLoggedInUserId<string>();
-    }
-}
