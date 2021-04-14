b6ceb50595911431d27372f84207e48ef869371d
Øistein Hay Syversen <oistein.hay.syversen@bekk.no>
2021-04-14 21:47:47 +0200

    Add script v2

---
+++ oh7.CspEditor.Core5.WebApp/variable-replacement-v2.ps1
@@ -0,0 +1,38 @@
+﻿Write-Host "Environment Variable Substitution"
+
+$prefix = ${env:APPLICATION_PREFIX} + "*"
+
+$props = gci env:$prefix | Select-Object -Property Name, Value
+
+$configPath = "$env:APPLICATION_PATH\$env:CONFIG_FILE"
+
+Write-Output "Loading config file from $configPath"
+$appSettings = Get-Content -Raw $configPath | ConvertFrom-Json
+
+foreach($variable in $props) {
+
+    $matchString = $variable.Name.replace(${env:APPLICATION_PREFIX}, "")
+
+    $matchProperties = $matchString.Split(".")
+
+    if($matchProperties.Count -gt 1) {
+        $match = $appSettings.($matchProperties[0]).psobject.properties | where { $_.Name -eq $matchProperties[1] } 
+        if ($match) {
+            $appSettings.($matchProperties[0]).($matchProperties[1]) = $variable.Value
+        }
+        else {
+            Write-Output "Could not find match for $matchString"
+        }
+    }
+    else {
+        $match = $appSettings.psobject.properties | where { $_.Name -eq $matchString } 
+        if ($match) {
+            $appSettings.($matchString) = $variable.Value
+        }
+        else {
+            Write-Output "Could not find match for $matchString"
+        }
+    }
+}
+
+$appSettings | ConvertTo-Json -depth 100 | Out-File $configPath
